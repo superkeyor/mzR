f379d9ad385ccdc9c6de1e447f0381e496c7de20
Jerry Zhu <jerryzhujian9@gmail.com>
2018-01-19 14:47:28 -0600

    update

--- R/mz.R
+++ R/mz.R
@@ -1,18 +1,9 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+#' preProcess + predict itself
+#' @description reProcess + predict itself
+#' @param x  x
+#' @export
+mz.precess = function(x,...){
+  preProcessObj=caret::preProcess(x,...)
+  return(predict(preProcessObj,x))
 }
